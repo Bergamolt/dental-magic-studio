73160a563ce3329ca14f9918452ff925f0eeab66
Bergamolt <bergamolt22@gmail.com>
2025-03-29 15:03:31 +0200

    Updated sendContact and sendAppointment functions to include headers for content type in fetch requests, ensuring proper data handling. Improved code formatting for better readability.

--- assets/vendor/email-form/validate.js
+++ assets/vendor/email-form/validate.js
@@ -1,94 +1,98 @@
 const ENDPOINT = "https://dental-magic-studio.bergamolt22.workers.dev/";
 
-// print "<p class='success'>Ваши данные отправлены.</p>";
-// } else {
-// print "<p class='Error'>Попробуйте еще раз.</p>";
-
 function sendContact() {
-	const valid = validateContact();
+  const valid = validateContact();
 
-	if(valid) {
-		fetch(ENDPOINT, {
-			method: "POST",
-			body: JSON.stringify({
-				userName: $("#userName").val(),
-				userTel: $("#userTel").val(), 
-				subject: $("#category").val()
-			})
-		})
-		.then(response => response.json())
-		.then(data => {
-			$("#mail-status").html(`<p class='success'>${data.message}</p>`);
-		})
-		.catch(error => {
-			if (error.statusMessage) {
-				$("#mail-status").html(`<p class='error'>${error.statusMessage}</p>`);
-			} else {
-				console.error('Error:', error);
-			}
-		});
-	}
+  if (valid) {
+    fetch(ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/form-data",
+      },
+      body: JSON.stringify({
+        userName: $("#userName").val(),
+        userTel: $("#userTel").val(),
+        subject: $("#category").val(),
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        $("#mail-status").html(`<p class='success'>${data.message}</p>`);
+      })
+      .catch((error) => {
+        if (error.statusMessage) {
+          $("#mail-status").html(`<p class='error'>${error.statusMessage}</p>`);
+        } else {
+          console.error("Error:", error);
+        }
+      });
+  }
 }
 
 function validateContact() {
-	let valid = true;	
-	$(".demoInputBox").css('background-color','');
-	$(".info").html('');
-	
-	if(!$("#userName").val()) {
-		$("#userName-info").html("(обязательно)");
-		$("#userName").css('border-color','#F42C04');
-		valid = false;
-	}
-	if(!$("#userTel").val()) {
-		$("#userTel-info").html("(обязательно)");
-		$("#userTel").css('border-color','#F42C04');
-		valid = false;
-	}
-	
-	return valid;
+  let valid = true;
+  $(".demoInputBox").css("background-color", "");
+  $(".info").html("");
+
+  if (!$("#userName").val()) {
+    $("#userName-info").html("(обязательно)");
+    $("#userName").css("border-color", "#F42C04");
+    valid = false;
+  }
+  if (!$("#userTel").val()) {
+    $("#userTel-info").html("(обязательно)");
+    $("#userTel").css("border-color", "#F42C04");
+    valid = false;
+  }
+
+  return valid;
 }
 
 function sendAppointment() {
-	const valid = validateAppointment();
-	if(valid) {
-		fetch(ENDPOINT, {
-		method: "POST",
-		body: JSON.stringify({
-			userName: $("#userNameA").val(),
-			userTel: $("#userTelA").val(),
-			subject: "Запись на прием"
-		})
-		})
-		.then(response => response.json())
-		.then(data => {
-			$("#mail-statusA").html(`<p class='success'>${data.message}</p>`);
-		})
-		.catch(error => {
-			if (error.statusMessage) {
-				$("#mail-statusA").html(`<p class='error'>${error.statusMessage}</p>`);
-			} else {
-				console.error('Error:', error);
-			}
-		});
-	}
+  const valid = validateAppointment();
+  if (valid) {
+    fetch(ENDPOINT, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/form-data",
+      },
+      body: JSON.stringify({
+        userName: $("#userNameA").val(),
+        userTel: $("#userTelA").val(),
+        subject: "Запись на прием",
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        $("#mail-statusA").html(`<p class='success'>${data.message}</p>`);
+      })
+      .catch((error) => {
+        if (error.statusMessage) {
+          $("#mail-statusA").html(
+            `<p class='error'>${error.statusMessage}</p>`
+          );
+        } else {
+          console.error("Error:", error);
+        }
+      });
+  }
 }
 
 function validateAppointment() {
-	let valid = true;	
-	$(".demoInputBox").css('background-color','');
-	$(".info").html('');
-	
-	if(!$("#userNameA").val()) {
-		$("#userNameA-info").html("(обязательно)");
-		$("#userNameA").css('border-color','#F42C04');
-		valid = false;
-	}
-	if(!$("#userTelA").val()) {
-		$("#userTelA-info").html("(обязательно)");
-		$("#userTelA").css('border-color','#F42C04');
-		valid = false;
-	}
-	
-	return valid;
+  let valid = true;
+  $(".demoInputBox").css("background-color", "");
+  $(".info").html("");
+
+  if (!$("#userNameA").val()) {
+    $("#userNameA-info").html("(обязательно)");
+    $("#userNameA").css("border-color", "#F42C04");
+    valid = false;
+  }
+  if (!$("#userTelA").val()) {
+    $("#userTelA-info").html("(обязательно)");
+    $("#userTelA").css("border-color", "#F42C04");
+    valid = false;
+  }
+
+  return valid;
 }
