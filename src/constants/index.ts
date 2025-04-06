d26a6bf6f3cede6b7dfecc3cd6fd96db57b534d9
Bergamolt <bergamolt22@gmail.com>
2025-04-06 13:50:03 +0300

    feat: implement contact form popup and update header with dynamic contact information

---
+++ src/constants/index.ts
@@ -0,0 +1,5 @@
+export const PHONE_NUMBER = "+380661981195";
+export const PHONE_NUMBER_MASK = "+38 (066) 198-11-95";
+export const ADDRESS = "Одеса, вул. Інглезі, 2А";
+export const GOOGLE_MAPS_LINK = "https://www.google.com.ua/maps/place/Dental+Magic+Studio/@46.4165096,30.7210305,17z/data=!3m1!4b1!4m6!3m5!1s0x40c633006971ab41:0x4e317edb1eeac409!8m2!3d46.4165059!4d30.7236054!16s%2Fg%2F11vrr8pdq4?hl=ru&entry=ttu&g_ep=EgoyMDI1MDMyNS4xIKXMDSoASAFQAw%3D%3D";
+export const EMAIL = "dentalmagicstudio@gmail.com";
